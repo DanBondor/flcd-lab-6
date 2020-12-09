434928d6395d85ff524a5a9bec4be0029263dc01
DanBondor <72183892+DanBondor@users.noreply.github.com>
2020-12-09 12:14:35 +0200

    Add files via upload

---
+++ lab6/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            MyGrammar myGrammar = new MyGrammar("C:\\Users\\Dan_B\\Documents\\FLC\\lab6\\src\\com\\company\\grammar.in");
+
+            while (true) {
+                System.out.println("1. See terminals\n2. See onnterminals\n3. See all productions\n4. See productions for a nonterminal\n5.See start symbol\n6. First Function");
+                Scanner in = new Scanner(System.in);
+                String option = in.nextLine();
+                if (option.equals("1")) {
+                    System.out.println(myGrammar.getTerminals());
+                }
+                if (option.equals("2")) {
+                    System.out.println(myGrammar.getNonterminals());
+                }
+                if (option.equals("3")) {
+                    System.out.println(myGrammar.getProductions());
+                }
+
+                if (option.equals("4")) {
+                    String nonterminal = in.nextLine();
+                    try {
+                        System.out.println(myGrammar.getProductionsForNonterminal(nonterminal));
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
+                }
+                if (option.equals("5")) {
+                    System.out.println(myGrammar.getFirst());
+                }
+                if (option.equals("6")) {
+                    String nonterminal = in.nextLine();
+                    System.out.println(myGrammar.getFirstB(nonterminal));
+                }
+                if (option.equals("7")) {
+                    String nonterminal = in.nextLine();
+                    System.out.println(myGrammar.getFollow(nonterminal));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ lab6/src/com/company/MyGrammar.java
@@ -0,0 +1,191 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.*;
+
+class MyGrammar {
+    private List<String> terminals;
+    private List<String> nonterminals;
+    private HashMap<String, List<List<String>>> productions;
+    private String first;
+    private List<List<String>> parsingTable;
+
+    MyGrammar(String filename) throws Exception {
+        this.terminals = new ArrayList<>();
+        this.nonterminals = new ArrayList<>();
+        this.productions = new HashMap<>();
+        this.parsingTable = new ArrayList<>();
+        this.readGrammar(filename);
+    }
+
+    List<String> getTerminals() {
+        return terminals;
+    }
+
+    List<String> getNonterminals() {
+        return nonterminals;
+    }
+
+    HashMap<String, List<List<String>>> getProductions() {
+        return productions;
+    }
+
+    String getFirst() {
+        return first;
+    }
+
+    private void readGrammar(String fileName) throws Exception {
+        File myObj = new File(fileName);
+        Scanner myReader;
+        try {
+            myReader = new Scanner(myObj);
+        } catch (FileNotFoundException e) {
+            System.out.println("No such file");
+            return;
+        }
+        String[] stateLine = myReader.nextLine().split(" ");
+        Collections.addAll(this.nonterminals, stateLine);
+
+        String[] alphaLine = myReader.nextLine().split(" ");
+        Collections.addAll(this.terminals, alphaLine);
+
+        this.first = myReader.nextLine();
+
+        if (!this.nonterminals.contains(this.first)) {
+            throw new Exception("Start symbol must be a non-terminal!");
+        }
+
+        while (myReader.hasNextLine()) {
+            String[] transSet = myReader.nextLine().split(",");
+            String nonterminal = transSet[0];
+            if (!this.nonterminals.contains(nonterminal)) {
+                throw new Exception(nonterminal + " is not a non-terminal!");
+            }
+            List<List<String>> transSetToAdd = new ArrayList<>();
+            for (int i = 1; i < transSet.length; i++) {
+                ArrayList<String> oneTransition = new ArrayList<>();
+                Collections.addAll(oneTransition, transSet[i].split(" "));
+                transSetToAdd.add(oneTransition);
+            }
+            checkProductionsSet(transSetToAdd);
+            this.productions.put(nonterminal, transSetToAdd);
+        }
+        myReader.close();
+    }
+
+    private void checkProductionsSet(List<List<String>> transSetToAdd) throws Exception {
+        for (List<String> e : transSetToAdd) {
+            for (String character : e) {
+                if (!terminals.contains(character) && !nonterminals.contains(character) && !character.equals("eps")) {
+                    throw new Exception(character + " is not a terminal!");
+                }
+            }
+        }
+    }
+
+    List<List<String>> getProductionsForNonterminal(String nonTerminal) throws Exception {
+        if (this.nonterminals.contains(nonTerminal))
+            return this.productions.get(nonTerminal);
+        throw new Exception(nonTerminal + " getProds: is not a nonterminal");
+    }
+
+
+    public List<String> getFirst(String nonterminal) throws Exception {
+        ArrayList<String> first = new ArrayList<>();
+        for (List<String> prod : getProductionsForNonterminal(nonterminal)) {
+            if (!first.contains(prod.get(0)) && !prod.get(0).equals(nonterminal)) {
+                first.add(prod.get(0));
+            }
+
+        }
+
+        for (int i = 0; i < first.size(); i++) {
+            if (this.nonterminals.contains(first.get(i))) {
+                List<String> newVars = new ArrayList<>();
+                for (List<String> prod : getProductionsForNonterminal(first.get(i))) {
+                    newVars.add(prod.get(0));
+                }
+                first.remove(i);
+
+                for (String el : newVars) {
+                    if (!first.contains(el) && !el.equals(nonterminal)) {
+                        first.add(el);
+                    }
+                }
+            }
+        }
+        return first;
+    }
+
+    public Set<String> getFirstB(String nonterminal) throws Exception {
+        if (!nonterminals.contains(nonterminal)) {
+            Set<String> arr = new HashSet<>();
+            arr.add(nonterminal);
+            return arr;
+        }
+
+
+        Set<String> arr = new HashSet<>();
+        for (List<String> prod : getProductionsForNonterminal(nonterminal)) {
+            if (!prod.get(0).equals(nonterminal)) {
+                arr.addAll(this.getFirstB(prod.get(0)));
+            }
+        }
+        return arr;
+    }
+
+    Set<String> getFollow(String nonterminal) throws Exception {
+        Set<String> follow = new HashSet<>();
+        if (nonterminal.equals(this.getFirst())) {
+            follow.add("$");
+            return follow;
+        }
+        for (String fromSymbol : this.getProductions().keySet()) {
+            for (List<String> prod : this.getProductions().get(fromSymbol)) {
+                for (int i = 0; i < prod.size(); i++) {
+                    if (prod.get(i).equals(nonterminal)) {
+                        if (i != prod.size() - 1) {
+                            if (this.nonterminals.contains(prod.get(i + 1))) {
+                                follow.addAll(this.getFirstB(prod.get(i + 1)));
+                            } else {
+                                follow.add(prod.get(i + 1));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return follow;
+    }
+
+    public List<List<String>> getParsingTable() throws Exception {
+        for (int nonterm = 0; nonterm < this.nonterminals.size(); nonterm++) {
+            List<String> row = new ArrayList<>();
+            if (shouldGetFollow(this.nonterminals.get(nonterm))) {
+                Set<String> symbols = this.getFollow(this.nonterminals.get(nonterm));
+                for (int term = 0; term < this.terminals.size(); term++) {
+                    if(symbols.contains(this.terminals.get(term))){
+                        row.add("some");
+                    }
+                    else{
+                        row.add("nada");
+                    }
+                }
+            }
+            else{
+
+            }
+            this.parsingTable.add(row);
+        }
+        return this.parsingTable;
+    }
+
+    public boolean shouldGetFollow(String nonterminal) throws Exception {
+        List<String> epsList = new ArrayList<>();
+        epsList.add("eps");
+        return getProductionsForNonterminal(nonterminal).contains(epsList);
+    }
+
+}
